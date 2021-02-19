60cb88383b86632741e728c90bbf8a7d3adf145a
TellomnaX <guillaume.montella@free.fr>
2021-02-19 22:40:24 +0100

    Version 0.1
    La boule grandit quand elle ramasse un objet
    La caméra est plus stable (mais toujours non fonctionnel)

--- GameJam-Sheep/Assets/Scripts/Player/PlayerItemDetection.cs
+++ GameJam-Sheep/Assets/Scripts/Player/PlayerItemDetection.cs
@@ -7,6 +7,12 @@ public class PlayerItemDetection : MonoBehaviour
     //Ce script a pour objectif de gérer la detection des moutons
 
     private Transform _SelfTransform;
+    private int _NumberOfCollectableObject = 0;
+
+    [SerializeField] private float _GrowFactor = 1.5f;
+    [SerializeField] private float _GrowSpeed = 2f;
+
+    private Vector3 _InitScale;
 
     void Awake()
     {
@@ -15,9 +21,14 @@ public class PlayerItemDetection : MonoBehaviour
     private void Initialisation()
     {
         _SelfTransform = transform;
+        _InitScale = _SelfTransform.localScale;
     }
 
-    private void OnTriggerEnter(Collider other)
+    void Update()
+    {
+        Growing();
+    }
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Stickable"))
         {
@@ -27,6 +38,18 @@ public class PlayerItemDetection : MonoBehaviour
 
     private void CatchNewElement(GameObject newElement)
     {
-        newElement.transform.parent = _SelfTransform;
+        _NumberOfCollectableObject++;
+        Destroy(newElement);
+    }
+    private void Growing()
+    {
+        Vector3 newScale = _InitScale + (new Vector3(_GrowFactor, _GrowFactor, _GrowFactor)* _NumberOfCollectableObject);
+        _SelfTransform.localScale = Vector3.Lerp(_SelfTransform.localScale, newScale, _GrowSpeed*Time.deltaTime);
+    }
+
+    //RETURNS
+    public int GetNumberOfCollectableObject()
+    {
+        return _NumberOfCollectableObject;
     }
 }
