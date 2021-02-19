ecb59a8c64d4f7173530dcc40c8c5af290103505
farcy <farcyalexandre.60@hotmail.fr>
2021-02-19 23:21:23 +0100

    ajout speed boost

---
+++ GameJam-Sheep/Assets/Scripts/SpeedBooster/SpeedBoost.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour
+{
+    public PlayerController speedControl;
+    private float defaultSpeed;
+    public float boostSpeed =12;
+
+    private void Start()
+    {
+        speedControl = gameObject.GetComponent<PlayerController>();
+      //  defaultSpeed = speedControl.
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag =="speedBoost")
+        {
+
+        }
+    }
+}
